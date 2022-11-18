e90064de2b7db64964afc810baf8a6c3c0d6fa1f
Izraquel <https://github.com/Izraquel>
2022-11-18 11:34:59 -0300

    finished

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color appBarColor = Color(0xFF2A2831);
+  static const Color primaryLightColor = Color(0xFF87A1FA);
+  static const Color primaryDarkColor = Color(0xFF5D78F0);
+  static const Color background = Colors.black;
+  static const Color white = Colors.white;
+  static Color black = Colors.black;
+}
